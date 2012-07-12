987ceb1d1168656f6d8476b8ec88c7ef29d69262
cleuton.sampaio@gmail.com <cleuton.sampaio@gmail.com@0e72ff09-c036-a4d3-37e1-fea569d44536>
2012-07-12 18:02:18 +0000

    branch 0.3

---
+++ branches/version_0-3_cleuton/AamoiOS/Aamo/AAmoScreenData.m
@@ -0,0 +1,16 @@
+//
+//  AAmoScreenData.m
+//  Aamo
+//
+//  Created by Cleuton Sampaio on 08/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AAmoScreenData.h"
+
+@implementation AAmoScreenData
+@synthesize uiid;
+@synthesize title;
+@synthesize onEndScript;
+@synthesize onLoadScript;
+@end
