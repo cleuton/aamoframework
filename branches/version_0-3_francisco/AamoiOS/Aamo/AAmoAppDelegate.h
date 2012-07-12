a2a75342230e86d0d5b4c3c0a8ce40141d830c59
cleuton.sampaio@gmail.com <cleuton.sampaio@gmail.com@0e72ff09-c036-a4d3-37e1-fea569d44536>
2012-07-12 18:02:53 +0000

    version 0.3

---
+++ branches/version_0-3_francisco/AamoiOS/Aamo/AAmoAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AAmoAppDelegate.h
+//  Aamo
+//
+//  Copyright (c) 2011 __The Code Bakers__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAmoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ branches/version_0-3_francisco/AamoiOS/Aamo/sairDaTela.lua
@@ -0,0 +1,2 @@
+-- Sai da tela
+aamo.exitScreen()
