7ce9a87292565b0e6453a198978aebbe8b756ca4
cleuton.sampaio <cleuton.sampaio@gmail.com>
2012-07-22 09:52:55 +0000

    v 0.3 onleave/onback

--- AamoDroid/src/org/thecodebakers/aamo/ScreenData.java
+++ AamoDroid/src/org/thecodebakers/aamo/ScreenData.java
@@ -7,5 +7,7 @@ public class ScreenData {
     public String title;
     public String onLoadScript;
     public String onEndScript; 
+    public String onLeaveScript;
+    public String onBackScript;
     public RelativeLayout dvLayout;
 }
