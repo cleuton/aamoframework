17ad88657eef9c01501c93f828398cf0b6d0b5f4
francisco.rodriges <francisco.rodriges@gmail.com>
2012-07-26 20:21:06 +0000

    inclusao das chamadas ao next

--- AamoDroid/assets/app/acessodb.lua
+++ AamoDroid/assets/app/acessodb.lua
@@ -1,10 +1,20 @@
-local cursor = {}
+-- local cursor = {}
 cursor = aamo.query("select * from contato")
 
-aamo.showMessage(cursor:getString(1))
-cursor:moveToNext()
-aamo.showMessage(cursor:getString(1))
-cursor:moveToNext()
-aamo.showMessage(cursor:getString(1))
-cursor:moveToNext()
--- aamo.showMessage(cursor:isAfterLast())
+for i=1,#cursor do
+   aamo.log(cursor[i])
+end
+
+-- next element
+cursor = aamo.next()
+
+--while ~ aamo.eof  --("contatos")
+for i=1,#cursor do
+   aamo.log(cursor[i])
+end
+
+--close the cursor 
+aamo.close()
+
+-- cursor:moveToNext()
+-- aamo.showMessage(cursor:getString(1))
