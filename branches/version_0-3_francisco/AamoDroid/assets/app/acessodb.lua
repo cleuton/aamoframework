3420c60c121d46037c58c22842d5554340eb81c1
francisco.rodriges@gmail.com <francisco.rodriges@gmail.com@0e72ff09-c036-a4d3-37e1-fea569d44536>
2012-07-27 14:22:39 +0000

    comentar os comandos Lua

--- branches/version_0-3_francisco/AamoDroid/assets/app/acessodb.lua
+++ branches/version_0-3_francisco/AamoDroid/assets/app/acessodb.lua
@@ -1,21 +1,19 @@
 -- local cursor = {}
+-- executa a query
 cursor = aamo.query("consulta","select * from contato")
 
+-- percorre os registros retornados
 while aamo.eof("consulta") do
-    aamo.log("entrou no while")
-	for i=1,#cursor do
+    -- imprime os campos do cursor
+    for i=1,#cursor do   
    		aamo.log(cursor[i])
 	end
 
 	cursor = aamo.next("consulta")
-	aamo.log("chamou o next")
+	
 end
 
 
 --close the cursor 
-aamo.log("vai fechar o cursor consulta")
 aamo.close("consulta")
 aamo.log("fechou o cursor consulta")
-
--- cursor:moveToNext()
--- aamo.showMessage(cursor:getString(1))
