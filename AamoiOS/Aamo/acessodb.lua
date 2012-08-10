cf8e0fbb44c00f3c23f9261318da4b818f58b2c7
francisco.rodriges <francisco.rodriges@gmail.com>
2012-08-10 01:05:02 +0000

    versao inicial

---
+++ AamoiOS/Aamo/acessodb.lua
@@ -0,0 +1,23 @@
+-- local cursor = {}
+-- executa a query
+cursor = aamo.query("consulta","select * from contato")
+
+-- percorre os registros retornados
+while not aamo.eof("consulta") do
+    -- imprime os campos do cursor
+    for i=1,#cursor do   
+   		aamo.log(cursor[i])
+	end
+
+	cursor = aamo.next("consulta")
+	
+end
+
+
+-- close the cursor 
+aamo.close("consulta")
+aamo.log("fechou o cursor consulta")
+
+-- close de database
+aamo.closeDatabase("consulta")
+aamo.log("fechou o banco de dados")
