38e19b431eee77c1b7aebc709c6c671db9232cc8
francisco.rodriges@gmail.com <francisco.rodriges@gmail.com@0e72ff09-c036-a4d3-37e1-fea569d44536>
2012-08-10 00:58:13 +0000

    versao inicial

---
+++ branches/version_0-3_francisco/AamoiOS/Aamo/AAmoColumn.m
@@ -0,0 +1,16 @@
+//
+//  AAmoColumn.m
+//  
+//
+//  Created by Cleuton Sampaio on 08/08/12.
+//  Copyright (c) 2012 Cleuton Sampaio. All rights reserved.
+//
+
+#import "AAmoColumn.h"
+
+@implementation AAmoColumn
+@synthesize name;
+@synthesize notNull;
+@synthesize primaryKey;
+@synthesize type;
+@end
