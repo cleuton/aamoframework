128c1600f8901d945ec3d82732d319afd04fc63d
cleuton.sampaio <cleuton.sampaio@gmail.com>
2012-09-05 12:04:30 +0000

    Versao 1.0 - Cleuton Antes do merge

---
+++ AamoDroid/src/org/thecodebakers/aamo/DynaView.java
@@ -0,0 +1,36 @@
+package org.thecodebakers.aamo;
+
+import java.util.List;
+
+import android.view.View;
+
+public class DynaView {
+	public enum CONTROL_TYPE {NONE, TEXTBOX, LABEL, BUTTON, CHECKBOX, LISTBOX, WEBBOX, IMAGEBOX};
+	public static int NOT_INITIALIZED = -1; 
+	public int screenId = NOT_INITIALIZED;
+	public int id = NOT_INITIALIZED;
+	public CONTROL_TYPE type = CONTROL_TYPE.NONE;
+    /*
+     1 - Textbox
+     2 - Label
+     3 - Button
+     4 - Checkbox
+     */
+	public float percentTop = NOT_INITIALIZED;
+	public float percentLeft = NOT_INITIALIZED;
+	public float percentHeight = NOT_INITIALIZED;
+	public float percentWidth = NOT_INITIALIZED;
+	public String url;
+	public boolean  checked;
+	public String text;
+	public String onCompleteScript;
+	public String onChangeScript;
+	public String onClickScript;
+	public String onElementSelected;
+	public String picture;
+	public boolean stretch;
+	
+	public List<String> listElements;
+	
+	public View view;
+}
---
+++ AamoiOS/Aamo/sairDaTela.lua
@@ -0,0 +1,3 @@
+-- Sai da tela
+aamo.showMessage(aamo.getGlobalParameter("p1"))
+aamo.exitScreen()
